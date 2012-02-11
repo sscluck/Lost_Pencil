562f4dbfb3d85e7270229c5f55d576b7057a59fb
Stephanie Luck <ssc.luck@gmail.com>
2012-02-11 13:14:59 -0500

    rejections allowed

--- db/migrate/20120211163323_add_posting_foundby.rb
+++ db/migrate/20120211163323_add_posting_foundby.rb
@@ -2,4 +2,8 @@ class AddPostingFoundby < ActiveRecord::Migration
   def self.up
     add_column :postings, :found_by, :integer
   end
+
+  def self.down
+    remove_column :postings, :found_by
+  end
 end
