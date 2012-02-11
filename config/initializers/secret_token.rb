d9d4693a974e244c95c5c4c7c93e8a4806275bb1
Stephanie Luck <ssc.luck@gmail.com>
2012-02-10 20:15:05 -0500

    updated readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LostPencil::Application.config.secret_token = '176b40b7ff8d7d34efd728d2e4f85ed270daa030244bd57503e150f5a2bc238f685a036071936ef931ac55d907e5c4bcc64ce124c8513fb707b96d2e2a0563e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LostPencil::Application.config.session_store :cookie_store, key: '_Lost_Pencil_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LostPencil::Application.config.session_store :active_record_store
